670f714986feca0ebe8b5415c08c45081fdb6e15
Juliana Souto <barutotec@gmail.com>
2022-10-17 18:18:20 -0300

    "Inserido a história do cantor Reginaldo Rossi"

--- app.py
+++ app.py
@@ -18,9 +18,35 @@ nome = "Reginaldo Rodrigues dos Santos"
 nome_art = "Reginaldo Rossi"
 data_nasc = "14 de fevereiro de 1944"
 data_fal = "20 de dezembro de 2013"
-nacional = "brasileiro"
+nacional = "Brasileiro"
 natural = "Recife/PE"
 # imagens
 
 nome_art = input("Digite o nome do artísta: ")
-print(f"{nome}, mais conhecido como {nome_art}, é {nacional}, nascido em {data_nasc} na cidade de {natural}. Faleceu no dia {data_fal}.")
+print()
+print(f"Nome completo: {nome}")
+print(f"Nascimento: {data_nasc}")
+print(f"Falecimento: {data_fal}")
+print(f"Nacionalidade: {nacional}")
+print(f"Naturalidade: {natural}")
+print()
+print(f"{nome_art} foi professor de matemática e estudou engenharia antes de iniciar sua carreira artística "
+      f"no ano de 1964, \ncomo integrante do conjunto The Silver Jets. \nEmbora ele tenha sido conhecido como o Rei do Brega, "
+      f"Reginaldo Rossi iniciou a sua carreira cantando Iê-Iê-Iê. \nSeu primeiro sucesso foi 'O Pão', também nome do seu "
+      f"primeiro disco. \nNa década de 70 Reginaldo Rossi se afastou do estilo rock com o trabalho 'À Procura de Você', onde deu início no gênero brega. "
+      f"\nSeu maior sucesso foi a música 'Mon Amour, Meu Bem, Ma Femme', regravada "
+      f"por vários outros artístas. \nNa década de 70 também houveram outros hits como 'Desterro' e 'Pedaço de Mau Caminho'."
+      f"\nNa década de 80 Reginaldo Rossi ganhou o seu primeiro disco de ouro com o álbum 'A Volta' e também lançou "
+      f"um de seus maiores sucessos, "
+      f"\na música 'Garçon', alcançando a marca de dois milhões de cópias vendidas e uma das primeiras músicas cantadas "
+      f"por vários outros cantores. \nEm fins da década de 90 Reginaldo Rossi ressurgiu no sul do país, relançando seus "
+      f"discos em CD.\nO cantor e compositor passou a ser visto como 'cult' e assinou contrato com a gravadora Sony. "
+      f"Na política, Reginaldo Rossi foi candidato a vereador \nno município de Jaboatão dos Guararapes em 2008 e deputado "
+      f"estadual em 2010, não obtendo êxito em nenhuma delas."
+      f"\nNo dia 09 de dezembro de 2013, Reginaldo Rossi passou por um procedimento médico retirando dois litros de líquido "
+      f"acumulados entre a pleura e o pulmão.\nO resultado da biópsia confirmou o diagnóstico de câncer de pulmão, doença "
+      f"que causou seu falecimento. Oito meses após o a sua morte, \nno dia 15 de agosto de 2014, sua viúva "
+      f"Celeide Neves também faleceu, aos 67 anos, de infarto.")
+
+
+
